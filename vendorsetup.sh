412513525c6fc016915f8e40e05d7d1a4c222ac3
nunhugger <jasonsouthward@gmail.com>
2013-07-12 11:00:07 -0500

    Revert "update to Poly"
    This reverts commit d70ee35a2749d92e079b1557220d83721dd8ac96.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,4 @@
-add_lunch_combo poly_d2att-userdebug
-add_lunch_combo poly_find5-userdebug
-add_lunch_combo poly_m7ul-userdebug
-add_lunch_combo poly_m7att-userdebug
-add_lunch_combo poly_mako-userdebug
-add_lunch_combo poly_quincyatt-userdebug
-add_lunch_combo poly_t0lteatt-userdebug
+for combo in $(curl -s https://raw.github.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-10.1 | awk {'print $1'})
+do
+    add_lunch_combo $combo
+done
